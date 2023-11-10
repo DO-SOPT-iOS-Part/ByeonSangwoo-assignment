34df047998d5c9420f8f2ea1861e7346191c112a
Sangwoo Byeon <tkddn0518@naver.com>
2023-11-10 21:39:59 +0900

    [Feat] #3 10일간 일기 예보 CollectionViewCell 구성 완료

---
+++ ByeonSangwoo-assignment/ViewControllers/Detail/TenDaysWeatherCollectionViewCell.swift
@@ -0,0 +1,100 @@
+//
+//  TenDaysWeatherCollectionViewCell.swift
+//  ByeonSangwoo-assignment
+//
+//  Created by 변상우 on 11/10/23.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class TenDaysWeatherCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier: String = "TenDaysWeatherCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setStyle()
+        self.setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private let day = UILabel()
+    private let weatherIconImage = UIImageView()
+    private let predictPercent = UILabel()
+    private let tempMin = UILabel()
+    private let tempImage = UIImageView()
+    private let tempMax = UILabel()
+    
+    func tenDaysWeatherBindData(data: TenDaysWeather) {
+        self.day.text = data.day
+        self.weatherIconImage.image = UIImage(systemName: data.weatherIconImage)
+        self.predictPercent.text = data.predictPercent
+        self.tempMin.text = data.tempMin
+        self.tempImage.image = UIImage(named: data.tempImage)
+        self.tempMax.text = data.tempMax
+    }
+    
+    private func setStyle() {
+        day.do {
+            $0.font = UIFont.medium(size: 22)
+        }
+        
+        weatherIconImage.do {
+            $0.contentMode = .scaleAspectFit
+            $0.tintColor = .white
+        }
+        
+        predictPercent.do {
+            $0.font = UIFont.bold(size: 15)
+        }
+        
+        tempMin.do {
+            $0.font = UIFont.medium(size: 22)
+            $0.textColor = .lightGray
+        }
+        
+        tempImage.do {
+            $0.contentMode = .scaleAspectFit
+        }
+        
+        tempMax.do {
+            $0.font = UIFont.medium(size: 22)
+        }
+    }
+    
+    private func setLayout() {
+        contentView.addSubviews(day, weatherIconImage, tempMin, tempImage, tempMax)
+        
+        day.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(26)
+        }
+        
+        weatherIconImage.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(100)
+            $0.height.equalTo(25)
+        }
+        
+        tempMin.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(weatherIconImage.snp.trailing).offset(15)
+        }
+        
+        tempImage.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(tempMin.snp.trailing).offset(8)
+            $0.height.equalTo(12)
+        }
+        
+        tempMax.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(tempImage.snp.trailing).offset(3)
+        }
+    }
+}
